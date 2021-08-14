9e7212cc1f479fdeff6569e9a7ccf184cb278f18
Shuvo Saha <shuvo.vertigo@gmail.com>
2021-08-15 00:02:33 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ echo "\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-base="light"\n\
-primaryColor="#c50839"\n\
-secondaryBackgroundColor="#efefef"\n\
-textColor="#01314c"\n\
+base='light'\n\
+primaryColor='#c50839'\n\
+secondaryBackgroundColor='#efefef'\n\
+textColor='#01314c'\n\
+\n\
 " > ~/.streamlit/config.toml
