f96ab3201ae11b26fb82bba2379df05ffca26bea
Shuvo Saha <shuvo.vertigo@gmail.com>
2021-08-14 23:46:10 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+base="light"\n\
+primaryColor="#c50839"\n\
+secondaryBackgroundColor="#efefef"\n\
+textColor="#01314c"\n\
+" > ~/.streamlit/config.toml
