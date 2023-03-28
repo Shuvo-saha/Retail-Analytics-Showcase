0ebf733fe877d9616ca65d905144b96fe20d6a5a
Shuvo Saha <shuvo.vertigo@gmail.com>
2023-03-28 22:01:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -401,7 +401,9 @@ with main2:
             ''')
 
         # if stacking classifier this doesn't work so make sure to use if function here
-        if model != stack:
+        if model == stack:
+            pass
+        else:
             text_fi = explain_weights(model, targets=[False, True], feature_names=['account length', 'international plan', 'voice mail plan',
                                                                                    'number vmail messages', 'total day minutes', 'total day calls',
                                                                                    'total day charge', 'total eve minutes', 'total eve calls',
